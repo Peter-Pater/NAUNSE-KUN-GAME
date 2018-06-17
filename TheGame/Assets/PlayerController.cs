44dee3cbc1133d62871505b4e17b8339bfc38d35
Weiyu <wangweiyu970219@sina.com>
2018-06-17 21:47:24 +0800

    addPlayerController

---
+++ TheGame/Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
