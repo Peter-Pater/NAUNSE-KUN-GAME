f8bfbb68c111c89cc9b1e14594b70eeebe824fa8
Xincheng Huang <xh721@nyu.edu>
2018-06-17 21:52:05 +0800

    Complete LeftDoor 1.0

---
+++ TheGame/Assets/Standard Assets/2D/Scripts/LeftDoorController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftDoorController : MonoBehaviour {
+
+    private float posY;
+    private int CLOSED = 0;
+    private int OPENED = 1;
+    private int status;
+    public bool upDownComplete;
+
+    // Use this for initialization
+	void Start () {
+        posY = transform.position.y;
+        status = CLOSED;
+        upDownComplete = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void moveUp(){
+        if (status == CLOSED){
+            upDownComplete = false;
+            float translation = Time.deltaTime;
+            if (transform.position.y - posY <= GetComponent<SpriteRenderer>().bounds.size.y)
+            {
+                transform.Translate(Vector2.up * translation);
+            }else{
+                status = OPENED;
+                posY = transform.position.y;
+            }    
+        }
+
+    }
+
+    public void moveDown(){
+        if (status == OPENED){
+            if (posY - transform.position.y <= GetComponent<SpriteRenderer>().bounds.size.y){
+                float translation = Time.deltaTime;
+                transform.Translate(Vector2.down * translation);        
+            }else{
+                status = CLOSED;
+                posY = transform.position.y;
+                upDownComplete = true;
+            }
+
+        }
+    }
+}
---
+++ TheGame/Assets/Standard Assets/2D/Scripts/SwitchController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour {
+
+    private bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp("space"))
+        {
+            triggered = true;
+        }
+
+        if (triggered == true){
+            // change color of object
+            GetComponent<SpriteRenderer>().material.color = Color.green;
+            // refer to the door script, and call move up
+            GameObject.FindGameObjectWithTag("LeftDoor").GetComponent<LeftDoorController>().moveUp();
+
+            // refer to the door script, and call move down
+            GameObject.FindGameObjectWithTag("LeftDoor").GetComponent<LeftDoorController>().moveDown();
+            if (GameObject.FindGameObjectWithTag("LeftDoor").GetComponent<LeftDoorController>().upDownComplete == true){
+                GetComponent<SpriteRenderer>().material.color = Color.white;
+                triggered = false;    
+            }
+        }
+	}
+
+	//private void OnTriggerEnter2D(Collider2D other){
+ //       if (other.gameObject.CompareTag("Player")){
+ //           if (Input.GetKeyUp("space")){
+ //               triggered = true;
+ //           }
+ //       }
+	//}
+}
