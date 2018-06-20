74647e72e268ec799ae5b4135ccf575f0895a33c
Sam Jianghao Hu <jh4828@nyu.edu>
2018-06-20 18:20:40 +0800

    Fixed player movement

---
+++ TheGame/Assets/Standard Assets/2D/Scripts/Player_Movement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    public float xSpeed = 0f; // how fast player moves. Can be adjusted in the spector.
+
+    Rigidbody2D myRigidbody;
+
+
+	private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+
+
+	void Update()
+    {
+        //var x = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
+        //var z = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        
+        //transform.Rotate(0, x, 0);
+        //transform.Translate(0, 0, z);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            myRigidbody.velocity = new Vector2(-xSpeed, myRigidbody.velocity.y);
+        }else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            myRigidbody.velocity = new Vector2(xSpeed, myRigidbody.velocity.y);      
+        }else
+        {
+            myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("DoorSwitch"))
+        {
+            //if (Input.GetKeyUp("space")){
+                GameObject.FindGameObjectWithTag("DoorSwitch").GetComponent<SwitchController>().triggered = true;
+            //}
+        }
+    }
+}
