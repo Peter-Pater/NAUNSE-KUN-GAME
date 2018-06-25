c615d1e70bff1fd64efda744ef51eeb2591f733a
Sam Jianghao Hu <jh4828@nyu.edu>
2018-06-26 02:03:31 +0800

    Added scene info

--- TheGame/Assets/Standard Assets/2D/Scripts/Camera_Movement.cs
+++ TheGame/Assets/Standard Assets/2D/Scripts/Camera_Movement.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class Camera_Movement : MonoBehaviour {
 
+	// scene state
+	int INSIDEKUN = 0;
+	int OUTSIDEKUN = 1;
+	public int currentScene = 0;
+
+
+	// scene object
+	public GameObject insideKUN;
+	public GameObject outsideKUN;
+	GameObject currentSceneObj;
+
+
+	// boarders that the camera will not move beyond
+	// will change depending on the current scene
+	float leftBoarder;
+	float rightBoarder;
+	float upBoarder;
+	float downBoarder;
+	float cameraWidth;
+	float cameraHeight;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 		
+		UpdateSceneObj();
+		UpdateSceneInfo();
+	}
+
+
+	void UpdateSceneObj(){
+		if (currentScene == INSIDEKUN){
+			currentSceneObj = insideKUN;
+		}else if (currentScene == OUTSIDEKUN){
+			currentSceneObj = outsideKUN;
+		}
+	}
+
+
+	void UpdateSceneInfo(){
+		Scene_Info targetInfo = currentSceneObj.GetComponent<Scene_Info>().
+
+		leftBoarder = targetInfo.leftBoarder;
+		rightBoarder = targetInfo.rightBoarder;
+		upBoarder = targetInfo.upBoarder;
+		downBoarder = targetInfo.downBoarder;
+		cameraWidth = targetInfo.cameraWidth;
+		cameraHeight = targetInfo.cameraHeight;
 	}
 }
---
+++ TheGame/Assets/Standard Assets/2D/Scripts/Scene_Info.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_Info : MonoBehaviour {
+
+	public float leftBoarder;
+	public float rightBoarder;
+	public float upBoarder;
+	public float downBoarder;
+	public float cameraWidth;
+	public float cameraHeight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
