dc9fff852c407a375149ba7c9161c41038e59a92
Sam Jianghao Hu <jh4828@nyu.edu>
2018-06-26 02:42:57 +0800

    Updated scene info for Scene 2

---
+++ TheGame/Assets/Standard Assets/2D/Scripts/Transition_toOutsideKUN.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition_toOutsideKUN : MonoBehaviour {
+
+    public Vector3 playerTargetPos;
+    public Vector3 cameraTargetPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
