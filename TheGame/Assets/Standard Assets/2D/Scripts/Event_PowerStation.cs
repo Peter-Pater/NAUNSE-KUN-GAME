606e54ec06d0d00afeb75f64a913f00245a69251
Sam Jianghao Hu <jh4828@nyu.edu>
2018-06-29 23:15:05 +0800

    Implemented stone dropping

--- TheGame/Assets/Standard Assets/2D/Scripts/Event_PowerStation.cs
+++ TheGame/Assets/Standard Assets/2D/Scripts/Event_PowerStation.cs
@@ -11,6 +11,7 @@ public class Event_PowerStation : MonoBehaviour {
 
     public Transform platformRotPoint;
     public Transform kunHead;
+    public Transform giantStone;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +24,11 @@ public class Event_PowerStation : MonoBehaviour {
         if (isRepaired){
             RotatePlatform();
             RotateAndMoveKUNHead();
+        }
+
+
+        if (kunHead.rotation.z <= 0.02f){
+            DropStone();
         }
 	}
 
@@ -44,6 +50,15 @@ public class Event_PowerStation : MonoBehaviour {
 
     void RotateAndMoveKUNHead(){
         kunHead.rotation = Quaternion.Slerp(kunHead.rotation, Quaternion.Euler(0, 0, 0), kunMovingSpeed * Time.deltaTime);
-        kunHead.position = Vector3.Lerp(kunHead.position, new Vector3(98, 1.3f, kunHead.position.z), kunMovingSpeed * Time.deltaTime);
+        kunHead.position = Vector3.Lerp(kunHead.position, new Vector3(98, 1f, kunHead.position.z), kunMovingSpeed * Time.deltaTime);
+    }
+
+
+    void DropStone(){
+        if (Mathf.Abs(giantStone.position.x - 7.7f) > 0.01f){
+            giantStone.position = new Vector3(7.7f, 3.6f, giantStone.position.z);
+        }
+
+        giantStone.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
