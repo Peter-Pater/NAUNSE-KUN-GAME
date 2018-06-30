54e158ebf2aa0d1401e7d539ec24ab69b1656269
Sam Jianghao Hu <jh4828@nyu.edu>
2018-07-01 00:23:57 +0800

    Implemented obtaining mountaineering pick and climbing up high wall

---
+++ TheGame/Assets/Standard Assets/2D/Scripts/Event_TreeWithBody.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_TreeWithBody : MonoBehaviour { // This scripts gets player mountaineering pick from the body on the tree.
+    
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+        if (collision.tag == "Player" && Input.GetKeyDown(KeyCode.Space)){
+            player.GetComponent<Player_Items>().whatsInHand = General_ItemList.MOUNTAINEERINGPICK;
+            Debug.Log("Mountaineering pick obtained!");
+        }
+	}
+}
