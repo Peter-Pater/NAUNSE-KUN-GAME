95e687cf67db7066627cf099393b42f4a553d4e3
Sam Jianghao Hu <jh4828@nyu.edu>
2018-07-03 00:24:37 +0800

    Implemented the point of no return in the cave

---
+++ TheGame/Assets/Standard Assets/2D/Scripts/Event_CaveNoReturn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_CaveNoReturn : MonoBehaviour { // This script prevents player from going back on slop in the cave.
+
+    public Transform playerTrans;
+    public GameObject cameraObj;
+
+    Collider2D myCollider2D;
+
+
+	// Use this for initialization
+	void Start () {
+        myCollider2D = GetComponent<Collider2D>();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+        // Once player passes this point in the cave,
+        // there'll be no way to go back!!
+        if (cameraObj.GetComponent<Camera_Movement>().currentScene == General_SceneList.CAVE){
+            if ((transform.position.x - playerTrans.position.x) >= 1f){
+                myCollider2D.isTrigger = false;
+            }
+        }
+
+	}
+}
