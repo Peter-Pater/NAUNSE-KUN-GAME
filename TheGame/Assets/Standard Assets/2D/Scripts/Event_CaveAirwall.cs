6c69831f2d8fe1e993bd952caee903dbf80a151c
Sam Jianghao Hu <jh4828@nyu.edu>
2018-07-02 23:45:02 +0800

    Implemented player getting blocked in cave when without flash light

---
+++ TheGame/Assets/Standard Assets/2D/Scripts/Event_CaveAirwall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_CaveAirwall : MonoBehaviour { // This script determines whether player can go through cave or not.
+
+    public GameObject player;
+
+    Collider2D myCollider2D;
+
+
+	// Use this for initialization
+	void Start () {
+        myCollider2D = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Let player go through if player holds flash light.
+        // Otherwise block player.
+        if (player.GetComponent<Player_Items>().whatsInHand == General_ItemList.FLASHLIGHT)
+        {
+            myCollider2D.isTrigger = true;
+        }else{
+            myCollider2D.isTrigger = false;
+        }
+	}
+}
---
+++ TheGame/Assets/Standard Assets/2D/Scripts/Event_CaveInOut.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_CaveInOut : MonoBehaviour { // This script sends player into cave.
+
+    public Transform cavePortal; // "Portal" that sends player to cave. 
+    public float portalStayTime; // How long will portal stay there until it goes back up.
+
+
+    Vector3 portalPrevPos; // Portal's original position.
+    bool timerStart = false;
+    float portalTimer;
+
+
+	// Use this for initialization
+	void Start () {
+        portalTimer = portalStayTime;
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+
+        // Start counting down when timer is on.
+        // Send portal back up again when time's up
+        // so that player will be able to access it again next time.
+        if (timerStart)
+        {
+            portalTimer -= Time.deltaTime;
+        }
+
+        if (portalTimer <= 0)
+        {
+            cavePortal.position = portalPrevPos;
+            portalTimer = portalStayTime;
+            timerStart = false;
+        }
+	}
+
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+        if (collision.tag == "Player" && Input.GetKeyDown(KeyCode.Space)){
+
+            // When player interacts with cave,
+            // move down the portal (which triggers transition when touching player)
+            // so that player touches it.
+            // Start the timer.
+            portalPrevPos = cavePortal.position;
+            cavePortal.position = transform.position;
+            timerStart = true;
+        }
+	}
+}
