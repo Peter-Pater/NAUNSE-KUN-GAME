b3f4b8c363f8d2d4015cb503a54de609cc982e20
Sam Jianghao Hu <jh4828@nyu.edu>
2018-07-18 01:21:42 +0800

    Fixed pushing tree

--- TheGame/Assets/Standard Assets/2D/Scripts/Event_PushTree.cs
+++ TheGame/Assets/Standard Assets/2D/Scripts/Event_PushTree.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class Event_PushTree : MonoBehaviour { // This script manages pushing tree.
 
-    public bool isCutDown = false;
+    public GameObject player;
+    public GameObject treeRotatePoint;
+    public bool isCutDown = false; // Keep track of whether the tree is cut down.
+
+    bool isPlayerPushing = false;
 
     Rigidbody2D myRigidbody;
+    Collider2D myCollider;
 
 
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         UpdateRigidbodyState();
+
+        if (isPlayerPushing){
+            myCollider.isTrigger = false;
+            if (Input.GetKeyDown(KeyCode.RightArrow)){
+                isPlayerPushing = false;
+            }
+        }else{
+            myCollider.isTrigger = true;
+        }
 	}
 
 
@@ -27,6 +42,17 @@ public class Event_PushTree : MonoBehaviour { // This script manages pushing tre
             myRigidbody.gravityScale = 1;
         }else{
             myRigidbody.gravityScale = 0;
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && Input.GetKeyDown(KeyCode.Space)){
+            if (isCutDown && !isPlayerPushing){
+                player.transform.position = new Vector3(transform.position.x + 4, player.transform.position.y, player.transform.position.z);
+                isPlayerPushing = true;
+            }
         }
     }
 }
